dbef82aad87e67636521d1b73764c047530be478
adel-daniel <adel.daniel.222@gmail.com>
2024-11-18 22:34:19 +0200

    Initial Commit

---
+++ memorizeTests/memorizeTests.swift
@@ -0,0 +1,16 @@
+//
+//  memorizeTests.swift
+//  memorizeTests
+//
+//  Created by Adel Nabil on 18/11/2024.
+//
+
+import Testing
+
+struct memorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
